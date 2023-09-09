f4fe2858d6cff8b03a914f817bed038a508e40bb
IshanGrover2004 <groverishan2004@gmail.com>
2023-09-09 13:38:37 +0530

    Add Docs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Password manager");
+    // Making a safe & secure, easy to use password manager and generator using clap and bcrypt
+
+    // TODO: Add clap for command line arguments
+
+    // TODO: Add bcrypt for hashing passwords
+
+    // TODO: Add file handling for storing passwords
+
+    // TODO: Add file handling for storing passwords
 }
