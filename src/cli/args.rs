5af8d7edc7c1cb4a9726cc543d8174c03758f233
IshanGrover2004 <groverishan2004@gmail.com>
2023-10-02 00:08:19 +0530

    feat: Restructure code to module system
    Organised code to module system in 3 parts: Cli, encrypt, store

---
+++ src/cli/args.rs
@@ -0,0 +1,106 @@
+use clap::{Args, Parser, Subcommand};
+// We can generate the password, make our own generator
+// use passwords::PasswordGenerator;
+
+#[derive(Parser)]
+#[clap(
+    name = "pass",
+    version,
+    author = "Ishan",
+    about = "A easy-to-use CLI password manager and generator"
+)]
+pub struct Cli {
+    /// Subcommand to do some operation like add, remove, etc.
+    #[command(subcommand)]
+    pub commands: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    /// Initialize the pass
+    Init(InitArgs),
+
+    /// Make a new password
+    Add(AddArgs),
+
+    /// Remove a password
+    Remove(RemoveArgs),
+
+    /// Update a password
+    Update(UpdateArgs),
+
+    /// List all made password
+    List(ListArgs),
+
+    /// Get a password
+    Get(GetArgs),
+}
+
+#[derive(Args)]
+pub struct InitArgs {
+    /// Master password for the pass
+    #[clap(required = false, default_value = "")]
+    master_password: String,
+}
+
+#[derive(Args)]
+pub struct AddArgs {
+    /// Master password required for authentication
+    #[clap(required = false, default_value = "")]
+    master_password: String,
+
+    /// Username/email of the account
+    #[clap(short = 'n', long = "name")]
+    username: String,
+
+    /// Password of the account (if not provided, a random password will be generated)
+    #[clap(short = 'p', long = "pass")]
+    password: Option<String>,
+
+    /// URL of the site/app for which the password is
+    #[clap(short = 'u', long = "url", default_value = "No URL provided")]
+    url: String,
+
+    /// Notes for the account
+    #[clap(short = 'm', required = false, default_value = " ")]
+    notes: String,
+}
+
+#[derive(Args)]
+pub struct RemoveArgs {
+    /// Master password required for authentication
+    #[clap(required = false, default_value = "")]
+    master_password: String,
+
+    /// Username/email of the account
+    // #[clap(short = 'n', long = "name")]
+    username: String,
+}
+
+#[derive(Args)]
+pub struct UpdateArgs {
+    /// Master password required for authentication
+    #[clap(required = false, default_value = "")]
+    master_password: String,
+
+    /// Username/email of the account
+    // #[clap(short = 'n', long = "name")]
+    username: String,
+}
+
+#[derive(Args)]
+pub struct ListArgs {
+    /// Master password required for authentication
+    #[clap(required = false, default_value = "")]
+    master_password: String,
+}
+
+#[derive(Args)]
+pub struct GetArgs {
+    // /// Master password required for authentication
+    // #[clap(default_value = "")]
+    // master_password: String,
+    /// Username/email of the account
+    // #[clap(short = 'n', long = "name")]
+    username: String,
+}
