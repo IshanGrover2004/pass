ddcd11ee55393c6b7229bb25901044112b085607
atamakahere <git@atamaka.maskmy.id>
2023-10-05 20:13:36 +0530

    fix: Master pass struct changes

--- src/pass/master.rs
+++ src/pass/master.rs
@@ -1,21 +1,26 @@
+use anyhow::Context;
+use colour::dark_blue;
 use once_cell::sync::Lazy;
 use std::marker::PhantomData;
+
+use crate::{encrypt::hash, store::pass::ask_master_password};
 
 // Making Base directories by xdg config
 const APP_NAME: &str = ".pass";
 const XDG_BASE: Lazy<xdg::BaseDirectories> = Lazy::new(|| {
     xdg::BaseDirectories::with_prefix(APP_NAME).expect("Failed to initialised XDG BaseDirectories")
 });
+const PASS_DIR_PATH: Lazy<std::path::PathBuf> = Lazy::new(|| XDG_BASE.get_data_home()); // $HOME/.local/share/.pass
 const MASTER_PASS_PATH: Lazy<std::path::PathBuf> =
-    Lazy::new(|| XDG_BASE.get_data_home().join(APP_NAME));
+    Lazy::new(|| XDG_BASE.place_data_file("Master.yml").unwrap()); // $HOME/.local/share/.pass/Master.yml
 
 // Initialising States & posibilities
-struct Uninit;
-struct Locked;
-struct Unlocked;
+pub struct Uninit;
+pub struct Locked;
+pub struct Unlocked;
 
 #[derive(Debug)]
-struct MasterPassword<State = Uninit> {
+pub struct MasterPassword<State = Uninit> {
     password_hash: Option<Vec<u8>>,
     unlocked_password: Option<Vec<u8>>,
     current_state: PhantomData<State>,
@@ -33,18 +38,49 @@ impl<Uninit> Default for MasterPassword<Uninit> {
 }
 
 // MasterPassword impl for UnInitialised state
-impl<Uninit> MasterPassword<Uninit> {
-    pub fn init(&mut self, hashed_password: String) -> MasterPassword<Locked> {
-        MasterPassword {
-            password_hash: None,
-            unlocked_password: None,
-            current_state: PhantomData::<Locked>,
+impl MasterPassword<Uninit> {
+    pub fn new() -> MasterPassword<Locked> {
+        // Check if pass not exist
+        if MASTER_PASS_PATH.exists() {
+            let hashed_password =
+                std::fs::read(MASTER_PASS_PATH.to_owned()).expect("Failed to read Master.yml file");
+
+            dark_blue!("Pass already initialised!!");
+
+            MasterPassword {
+                password_hash: Some(hashed_password),
+                unlocked_password: None,
+                current_state: PhantomData::<Locked>,
+            }
+        }
+        // if not exist create & ask user for master_password
+        else {
+            if !PASS_DIR_PATH.exists() {
+                std::fs::create_dir_all(PASS_DIR_PATH.to_owned())
+                    .context("Failed to create pass directory!!")
+                    .unwrap();
+            }
+
+            let prompt_master_password = ask_master_password();
+            let hashed_password = hash(&prompt_master_password);
+
+            std::fs::write(MASTER_PASS_PATH.to_owned(), &hashed_password)
+                .context("Failed to write master password!")
+                .unwrap();
+
+            dark_blue!("Pass initialised successfully!!");
+
+            MasterPassword {
+                password_hash: Some(hashed_password.as_bytes().to_vec()),
+                unlocked_password: Some(prompt_master_password.as_bytes().to_vec()),
+                current_state: PhantomData,
+            }
         }
     }
 }
 
 // MasterPassword impl for Locked state
-impl<Locked> MasterPassword<Locked> {
+impl MasterPassword<Locked> {
     // Unlock the master password
     pub fn unlock(&self, prompt_master_password: &str) -> MasterPassword<Unlocked> {
         MasterPassword {
@@ -55,6 +91,7 @@ impl<Locked> MasterPassword<Locked> {
     }
 }
 
+/*
 // MasterPassword impl for Unlocked state
 impl<Unlocked> MasterPassword<Unlocked> {
     // Lock the master password & stop from accessing it
@@ -74,3 +111,4 @@ impl<Unlocked> MasterPassword<Unlocked> {
         String::from("ndbhbx")
     }
 }
+*/
