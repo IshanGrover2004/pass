77f0550978902c9cf09439003d6edef99f550c49
IshanGrover2004 <groverishan2004@gmail.com>
2023-11-19 19:58:22 +0530

    feat: Add `search` command  to fuzzy find by service

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -160,6 +160,35 @@ pub fn run_cli(master_password: MasterPassword<Init>) -> anyhow::Result<()> {
             }
         }
 
+        Some(Command::Search(args)) => {
+            let mut master = master_password.load()?;
+
+            for attempt in 0..3 {
+                master.borrow_mut().prompt()?;
+
+                match master.verify() {
+                    Ok(Some(verified)) => {
+                        args.fuzzy_search(verified)?;
+                        break;
+                    }
+                    Ok(None) => {
+                        if attempt < 2 {
+                            colour::e_red_ln!(
+                                "Incorrect master password, retry ({}):",
+                                2 - attempt
+                            );
+                        } else {
+                            colour::e_red_ln!("Wrong master password");
+                        }
+                    }
+                    Err(e) => {
+                        e_red_ln!("Unable to add password due to: {}", e.to_string());
+                        break;
+                    }
+                };
+            }
+        }
+
         Some(Command::Gen(args)) => {
             args.generate_password();
         }
